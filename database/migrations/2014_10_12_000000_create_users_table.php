52f632a1e609df4f8cc258cad450ee26a74368f5
Paul Fruitful <fruitful2007@outlook.com>
2022-07-13 16:36:50 +0100

    New Changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->foreignId('blogs')->reference('title')->on('blogs')->onDelete('cascade');
-            $table->string('profile_photo_path', 2048)->nullable();
+            
             $table->timestamps();
         });
     }
