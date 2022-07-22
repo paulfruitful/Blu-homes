32b20c863a46a00fbcb954258cad83f37358e543
Paul Fruitful <fruitful2007@outlook.com>
2022-07-22 01:09:17 +0100

    New features, change of the dasjboard UI

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,5 @@
+<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -5,5 +7,10 @@
         </h2>
     </x-slot>
 
- {{ $blogs}}
+  <div class="row">
+      <div class="col-6">
+          <a href="/blog/create" class="btn btn-primary">Create New Blog</a>
+      </div>
+  </div>
+
 </x-app-layout>
