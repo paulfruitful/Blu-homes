805bc8bd0c10fd4f4cbed1387630c68cd40fb701
Paul Fruitful <fruitful2007@outlook.com>
2022-08-27 23:33:44 +0100

    Fixing the user dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-     $user= \App\Models\User::factory()->create([
-        'name' => 'Test User',
-             'email' => 'test@example.com'
+    // $user= \App\Models\User::factory()->create([
+      //  'name' => 'Test User',
+         //    'email' => 'test@example.com'
 
-     ]);
+     //]);
      \App\Models\blog::factory(17)->create([
         'user_id'=>2
      ]);
