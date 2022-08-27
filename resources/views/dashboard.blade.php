b39a147365d419195726ced61919a49b36375973
Paul Fruitful <fruitful2007@outlook.com>
2022-08-27 23:59:33 +0100

    Fixing the user dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
         </h2>
     </x-slot>
 
-                        <div class="container-sm">
+                        <div class="container">
                     <div class="row">
                         <div class="col align-self-center">
           <a href="/blog/create" class="btn btn-primary">Create New Blog</a></div>
