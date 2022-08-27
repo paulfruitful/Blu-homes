6326e1358ccc3e949f6d12ec9a8f37877a606c11
Paul Fruitful <fruitful2007@outlook.com>
2022-08-27 23:39:13 +0100

    Fixing the user dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
 
   <div class="row">
       <div class="col-12">
-        <div class="col-3">
+        <div class="col-6">
           <a href="/blog/create" class="btn btn-primary">Create New Blog</a>
     </div>  </div>
   </div>
