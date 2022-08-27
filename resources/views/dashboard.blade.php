bff139c0816e1d7d2bb4869bb36267eea9f94717
Paul Fruitful <fruitful2007@outlook.com>
2022-08-27 23:35:00 +0100

    Fixing the user dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,5 +12,21 @@
           <a href="/blog/create" class="btn btn-primary">Create New Blog</a>
       </div>
   </div>
+  @if(count($blogs)>=1)
+@foreach ($blogs as $blog)
+
+   <div class="card">
+        <div class="card-body">
+        <h3 class="card-title"><a href="/blog/{{$blog->id}}">{{$blog->title}}</a></h3>
+        <small class="card-body">Written at {{$blog->created_at}}</small>
+</div>
+    </div>
+@endforeach
+
+@endif
+<div class="d-flex justify-content-center">
+    {!! $blogs->links() !!}
+</div>
+
 
 </x-app-layout>
