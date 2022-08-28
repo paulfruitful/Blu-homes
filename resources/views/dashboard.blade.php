a2e4da54e59bd88df17b365532e48b0d03e88fe4
Paul Fruitful <fruitful2007@outlook.com>
2022-08-28 21:37:26 +0100

    Fixing the user dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,7 +18,7 @@
 <div class="container-sm">
    <div class="card">
         <div class="card-body">
-        <h3 class="card-title"><a href="/blog/{{$blog->id}}">{{$blog->title}}</a></h3>
+        <h3 class="card-title"><a href="/blog/{{$blog->id}}">{{$blog->title}}</a></h3><a href="/blog/{{$blog->id}}/edit" class="btn btn-prmary align-self-end">Edit Post</a>
         <small class="card-body">Written at {{$blog->created_at}}</small>
 </div>
     </div>
