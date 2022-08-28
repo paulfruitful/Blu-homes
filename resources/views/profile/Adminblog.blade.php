c4e84dacda87a35937dd744c9bdcdfe0e0768c39
Paul Fruitful <fruitful2007@outlook.com>
2022-08-28 21:41:02 +0100

    Fixing the user dashboard

---
+++ resources/views/profile/Adminblog.blade.php
@@ -0,0 +1,8 @@
+<head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
